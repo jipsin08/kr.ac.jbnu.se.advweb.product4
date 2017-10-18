4fa42e6fa8ed223dfea0b1d4194c4e56e7b83b6e
kimmj0920 <31896888+kimmj0920@users.noreply.github.com>
2017-10-18 21:16:07 +0900

    미완성 LoginServlet2

---
+++ src/kr/ac/jbnu/se/advweb/product/servlet/LoginServlet2.java
@@ -0,0 +1,31 @@
+package kr.ac.jbnu.se.advweb.product.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login2")
+public class LoginServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet2() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher 
+				= this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView2.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
